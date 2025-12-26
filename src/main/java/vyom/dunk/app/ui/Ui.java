dc00ed26ce60f44fbe93c8efb5753e90cd6d32d2
Milton Ytusaca <milton.yv98@gmail.com>
2025-12-26 10:26:18 -0500

    menu2

--- src/main/java/vyom/dunk/app/ui/Ui.java
+++ src/main/java/vyom/dunk/app/ui/Ui.java
@@ -1,17 +1,21 @@
 package vyom.dunk.app.ui;
 
 import java.util.Scanner;
-
-import vyom.dunk.app.utils.WriteText;
+import java.io.Console;
 
 public class Ui {
   private static String title = "Project x";
+  private static String subtitle = "";
+  private static boolean isLoggin = false;
 
   static Scanner sc = new Scanner(System.in);
 
   public static void render() {
     clearScreen();
     System.out.println("Bienenido a " + title);
+    if (subtitle != "")
+      System.out.println(subtitle);
+
     System.out.println();
   }
 
@@ -19,17 +23,17 @@ public class Ui {
 
     while (true) {
       render();
-      System.out.println("1) Iniciar sesion.");
-      System.out.println("2) Registrarse.");
-      System.out.println("3) Idioma.");
-      System.out.println("4) Salir.");
+      System.out.println("1)Iniciar sesion.");
+      System.out.println("2)Registrarse.");
+      System.out.println("3)Idioma.");
+      System.out.println("4)Salir.");
 
       System.out.print("Ingrese opcion: ");
       String opt = sc.nextLine().trim();
 
       switch (opt) {
         case "1" -> login();
-        case "2" -> System.out.println("Conectando con la base de datos");
+        case "2" -> register();
         case "3" -> System.out.println("No hay mas idiomas disponibles XD");
         case "4" -> {
           System.out.println("Saliendo...");
@@ -42,24 +46,68 @@ public class Ui {
 
   public static void login() {
     clearScreen();
+    subtitle = "Login";
     render();
 
     System.out.print("Ingrese nickname: ");
     String nick = sc.nextLine().trim();
 
-    WriteText.write(nick);
-    System.out.print("Ingrese contrasena: ");
-    String password = sc.nextLine().trim();
+    Console console = System.console();
+
+    console.printf("Ingrese su contrasena: ");
+    char[] passwordChars = console.readPassword();
+    String passwordString = new String(passwordChars);
+
+    console.printf(passwordString + "\n");
+
+    isLoggin = true;
+
+    if (true)
+      menu2();
   }
 
   public static void register() {
-    System.out.print("Ingrese nickname: ");
-    String nick = sc.nextLine().trim();
-    System.out.println();
+    Console console = System.console();
+
+    console.printf("Ingrese su nickname: ");
+    String nickname = console.readLine();
+
+    console.printf("Ingrese su correo(incluir @gmail): ");
+    String email = console.readLine();
+
+    console.printf("Ingrese su contrasena");
+    char[] passwordChars = console.readPassword();
+    String passwordString = new String(passwordChars);
+
+    console.printf("Usuario creado!");
+    console.printf(nickname + "|" + email);
   }
 
   static void clearScreen() {
     System.out.print("\033[H\033[2J");
     System.out.flush();
+  }
+
+  // Si el inicio de sesion es exitoso muestra este menu
+  static void menu2() {
+    clearScreen();
+    render();
+
+    while (true) {
+      System.out.println("1)Iniciar partida");
+      System.out.println("2)Continuar partida");
+      System.out.println("3)Volver al menu principal");
+
+      System.out.print("Ingrese opcion: ");
+      String opt = sc.nextLine().trim();
+
+      switch (opt) {
+        case "1" -> System.out.println("Iniciando partida...");
+        case "2" -> System.out.println("Aun no esta disponible XD");
+        case "3" -> {
+          return;
+        }
+      }
+    }
   }
 }
