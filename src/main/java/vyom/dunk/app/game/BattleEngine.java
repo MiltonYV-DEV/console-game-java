6bcc7609d72eb05a985139f55292bbcf4c294d7d
Milton Ytusaca <milton.yv98@gmail.com>
2025-12-30 11:50:12 -0500

    add BaseCharacter in Player

--- src/main/java/vyom/dunk/app/game/BattleEngine.java
+++ src/main/java/vyom/dunk/app/game/BattleEngine.java
@@ -16,12 +16,38 @@ public class BattleEngine {
     System.out.println(player.getName() + " vs " + enemy.getName() + "\n");
 
     while (player.isAlive() && enemy.isAlive()) {
+      printStatus(player, enemy);
+      System.out.println("Tu turno: ");
+      System.out.println("1) Atacar");
+      System.out.println("2) Curar (+10)");
+      System.out.print("> ");
 
+      String opt = sc.nextLine().trim();
+
+      switch (opt) {
+        case "1" -> {
+          int dmg = rollDamage(8, 12);
+          enemy.takeDamage(dmg);
+          System.out.println("Te curaste e hiciste " + dmg + " de dano.");
+        }
+
+        case "2" -> {
+          int healted = player.heal(10);
+          System.out.println("Te curaste " + healted + " HP.");
+        }
+
+      }
     }
   }
 
+  private int rollDamage(int min, int max) {
+    return rng.nextInt((max - min) + 1) + min;
+  }
+
   private void printStatus(Player p, Enemy e) {
-    System.out.println("--- Estado ---");
+    System.out.println("----- Estado -----");
     System.out.println(p.getName() + ": " + p.getHp() + "/" + p.getMaxHp());
+    System.out.println(e.getName() + ": " + e.getHp() + "/" + e.getMaxHp());
+    System.out.println("----------------");
   }
 }
--- src/main/java/vyom/dunk/app/models/Player.java
+++ src/main/java/vyom/dunk/app/models/Player.java
@@ -1,47 +1,14 @@
 package vyom.dunk.app.models;
 
-public class Player {
-  private final String name;
-  private int maxHP;
-  private int hp;
-  private int potions;
-  private int attack;
+public class Player extends BaseCharacter {
 
-  public Player(String name) {
-    if (name == null || name.isBlank())
-      throw new IllegalArgumentException("Nombre invalido");
-
-    this.name = name;
-    this.hp = 100;
-    this.maxHP = 100;
-    this.potions = 4;
-    this.attack = 10;
-  }
-
-  // Getters
-  public String getName() {
-    return name;
-  }
-
-  public int getHp() {
-    return hp;
-  }
-
-  public int getMaxHp() {
-    return this.maxHP;
-  }
-
-  public boolean isAlive() {
-    return hp > 0;
+  public Player(String name, int hp) {
+    super(name, hp);
   }
 
   // Turno
   public void startTurn() {
 
-  }
-
-  public int getAttack() {
-    return this.attack;
   }
 
   // efectos
@@ -50,9 +17,10 @@ public class Player {
       incomingDamage = 0;
 
     int reduced = Math.max(0, incomingDamage);
-    int oldHp = hp;
-    hp = Math.max(0, hp - reduced);
+    int oldHp = this.getHp();
+    int newHp = Math.max(0, this.getHp() - reduced);
 
-    return oldHp - hp;
+    this.setHp(newHp);
+    return oldHp - this.getHp();
   }
 }
