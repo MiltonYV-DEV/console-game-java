340bd76abbca5dbe84c10d559a60ea9bd7b4ba43
Milton Ytusaca <milton.yv98@gmail.com>
2026-01-25 13:32:30 -0500

    sql

--- database.sql
+++ database.sql
@@ -1,4 +1,4 @@
-  enemy_name VADROP SCHEMA  poo_pf;
+DROP SCHEMA  poo_pf;
 CREATE SCHEMA poo_pf;
 USE poo_pf;
 
@@ -47,19 +47,13 @@ CREATE TABLE battles (
   id INT AUTO_INCREMENT PRIMARY KEY,
   match_id INT NOT NULL,
   enemy_source ENUM('BASE','AI') NOT NULL,
-  enemy_id INT NULL,
   generated_enemy_id INT NULL,
   result ENUM('WIN','LOSE', 'ABANDONED') DEFAULT 'ABANDONED',
   turns INT NOT NULL DEFAULT 0,
   damage_dealt INT NOT NULL DEFAULT 0,
   damage_taken INT NOT NULL DEFAULT 0,
   CONSTRAINT fk_battle_match FOREIGN KEY (match_id) REFERENCES matches(id),
-  CONSTRAINT fk_battle_gen_enemy FOREIGN KEY (generated_enemy_id) REFERENCES generated_enemies(id),
-  CONSTRAINT chk_enemy_ref CHECK (
-    (enemy_source='BASE' AND enemy_id IS NOT NULL AND generated_enemy_id IS NULL)
-    OR
-    (enemy_source='AI' AND generated_enemy_id IS NOT NULL AND enemy_id IS NULL)
-  )
+  CONSTRAINT fk_battle_gen_enemy FOREIGN KEY (generated_enemy_id) REFERENCES generated_enemies(id)
 );
 
 # modificacions
@@ -195,37 +189,37 @@ INSERT INTO generated_enemies (match_id, prompt_user_text, enemy_name, enemy_lev
  JSON_OBJECT('hp', 110, 'attack', 24, 'defense', 5, 'element', 'none'));
 
 INSERT INTO battles (
-  match_id, enemy_source, enemy_id, generated_enemy_id, result,
+  match_id, enemy_source, generated_enemy_id, result,
   turns, damage_dealt, damage_taken
 ) VALUES
-(1, 'AI', NULL, 1, 'WIN',       8, 140, 70),
-(2, 'AI', NULL, 2, 'ABANDONED', 3,  35, 20),
-(3, 'AI', NULL, 3, 'LOSE',     11, 120, 160),
-(4, 'AI', NULL, 4, 'WIN',       6, 115, 55),
-(5, 'AI', NULL, 5, 'WIN',      14, 230, 150),
-(6, 'AI', NULL, 6, 'LOSE',      9,  98, 130),
-(7, 'AI', NULL, 1, 'WIN',       8, 140, 70),
-(8, 'AI', NULL, 2, 'ABANDONED', 3,  35, 20),
-(9, 'AI', NULL, 3, 'LOSE',     11, 120, 160),
-(10, 'AI', NULL, 4, 'WIN',       6, 115, 55),
-(11, 'AI', NULL, 5, 'WIN',      14, 230, 150),
-(12, 'AI', NULL, 6, 'LOSE',      9,  98, 130),
-(13, 'AI', NULL, 1, 'WIN',       8, 140, 70),
-(14, 'AI', NULL, 2, 'ABANDONED', 3,  35, 20),
-(15, 'AI', NULL, 3, 'LOSE',     11, 120, 160),
-(16, 'AI', NULL, 4, 'WIN',       6, 115, 55),
-(17, 'AI', NULL, 5, 'WIN',      14, 230, 150),
-(18, 'AI', NULL, 6, 'LOSE',      9,  98, 130),
-(19, 'AI', NULL, 1, 'WIN',       8, 140, 70),
-(20, 'AI', NULL, 2, 'ABANDONED', 3,  35, 20),
-(21, 'AI', NULL, 3, 'LOSE',     11, 120, 160),
-(22, 'AI', NULL, 4, 'WIN',       6, 115, 55),
-(23, 'AI', NULL, 5, 'WIN',      14, 230, 150),
-(24, 'AI', NULL, 6, 'LOSE',      9,  98, 130),
-(25, 'AI', NULL, 1, 'WIN',       8, 140, 70),
-(26, 'AI', NULL, 2, 'ABANDONED', 3,  35, 20),
-(27, 'AI', NULL, 3, 'LOSE',     11, 120, 160),
-(28, 'AI', NULL, 4, 'WIN',       6, 115, 55);
+(1, 'AI', 1, 'WIN',       8, 140, 70),
+(2, 'AI', 2, 'ABANDONED', 3,  35, 20),
+(3, 'AI', 3, 'LOSE',     11, 120, 160),
+(4, 'AI', 4, 'WIN',       6, 115, 55),
+(5, 'AI', 5, 'WIN',      14, 230, 150),
+(6, 'AI', 6, 'LOSE',      9,  98, 130),
+(7, 'AI', 1, 'WIN',       8, 140, 70),
+(8, 'AI', 2, 'ABANDONED', 3,  35, 20),
+(9, 'AI', 3, 'LOSE',     11, 120, 160),
+(10, 'AI', 4, 'WIN',       6, 115, 55),
+(11, 'AI', 5, 'WIN',      14, 230, 150),
+(12, 'AI', 6, 'LOSE',      9,  98, 130),
+(13, 'AI', 1, 'WIN',       8, 140, 70),
+(14, 'AI', 2, 'ABANDONED', 3,  35, 20),
+(15, 'AI', 3, 'LOSE',     11, 120, 160),
+(16, 'AI', 4, 'WIN',       6, 115, 55),
+(17, 'AI', 5, 'WIN',      14, 230, 150),
+(18, 'AI', 6, 'LOSE',      9,  98, 130),
+(19, 'AI', 1, 'WIN',       8, 140, 70),
+(20, 'AI', 2, 'ABANDONED', 3,  35, 20),
+(21, 'AI', 3, 'LOSE',     11, 120, 160),
+(22, 'AI', 4, 'WIN',       6, 115, 55),
+(23, 'AI', 5, 'WIN',      14, 230, 150),
+(24, 'AI', 6, 'LOSE',      9,  98, 130),
+(25, 'AI', 1, 'WIN',       8, 140, 70),
+(26, 'AI', 2, 'ABANDONED', 3,  35, 20),
+(27, 'AI', 3, 'LOSE',     11, 120, 160),
+(28, 'AI', 4, 'WIN',       6, 115, 55);
 
 
 
@@ -305,212 +299,10 @@ GROUP BY u.id, u.username
 ORDER BY wins DESC, total_matches DESC
 LIMIT ?;
 
+# modificaciones
+ALTER TABLE characters
+  ADD UNIQUE KEY uq_characters_user_id (user_id);
 
-RCHAR(80) NOT NULL,
-  enemy_level INT NOT NULL,
-  stats_json JSON NOT NULL,
-  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  CONSTRAINT fk_gen_match FOREIGN KEY (match_id) REFERENCES matches(id)
-);
-
-CREATE TABLE battles (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  match_id INT NOT NULL,
-  enemy_source ENUM('BASE','AI') NOT NULL,
-  enemy_id INT NULL,
-  generated_enemy_id INT NULL,
-  result ENUM('WIN','LOSE', 'ABANDONED') DEFAULT 'ABANDONED',
-  turns INT NOT NULL DEFAULT 0,
-  damage_dealt INT NOT NULL DEFAULT 0,
-  damage_taken INT NOT NULL DEFAULT 0,
-  CONSTRAINT fk_battle_match FOREIGN KEY (match_id) REFERENCES matches(id),
-  CONSTRAINT fk_battle_gen_enemy FOREIGN KEY (generated_enemy_id) REFERENCES generated_enemies(id),
-  CONSTRAINT chk_enemy_ref CHECK (
-    (enemy_source='BASE' AND enemy_id IS NOT NULL AND generated_enemy_id IS NULL)
-    OR
-    (enemy_source='AI' AND generated_enemy_id IS NOT NULL AND enemy_id IS NULL)
-  )
-);
-
-# modificacions
-ALTER TABLE battles
-    MODIFY result ENUM('WIN', 'LOSE', 'ABANDONED') DEFAULT 'ABANDONED';
-
-#Usuarios DEMO
-INSERT INTO users (username, password_hash) VALUES
-('panchito',   '$2b$12$demo_hash_vyom_1234567890abcdef'),
-('pedrito',  '$2b$12$demo_hash_maria_1234567890abcd'),
-('luchito', '$2b$12$demo_hash_carlos_1234567890abc'),
-('pablito',   '$2b$12$demo_hash_luis_1234567890abca'),
-('anita',    '$2b$12$demo_hash_ana_1234567890abcb');
-
-# Characters DEMO
-INSERT INTO characters (user_id, name, level, hp, attack, defense) VALUES
-(1, 'RangerRojo',     3, 130, 18,  8),
-(2, 'RangerAzul',     2, 115, 14,  6),
-(3, 'RangerVerde',    5, 160, 22, 12),
-(4, 'RangerNegro',    1, 100, 10,  5),
-(5, 'Beyota',    4, 145, 20,  9);
-
-# Matches DEMO
-INSERT INTO matches (user_id, started_at, ended_at, result, total_turns, xp_gained) VALUES
-(1, '2026-01-20 18:10:00', '2026-01-20 18:22:00', 'WIN',       12, 120),
-(1, '2026-01-22 21:00:00', NULL,                  'ABANDONED',         0,   0),
-(2, '2026-01-21 19:05:00', '2026-01-21 19:19:00', 'LOSE',      10,  30),
-(3, '2026-01-18 15:40:00', '2026-01-18 16:05:00', 'ABANDONED',  6,   0),
-(4, '2026-01-23 10:00:00', '2026-01-23 10:14:00', 'WIN',        9,  80),
-(5, '2026-01-24 09:30:00', NULL,                  'ABANDONED',         0,   0),
-(1,'2026-01-22 18:10:00', '2026-01-22 18:12:00', 'WIN', 10, 120),
-(1, '2026-01-20 18:10:00', '2026-01-20 18:22:00', 'WIN',       12, 120),
-(1, '2026-01-22 21:00:00', NULL,                  'ABANDONED',         0,   0),
-(2, '2026-01-21 19:05:00', '2026-01-21 19:19:00', 'LOSE',      10,  30),
-(3, '2026-01-18 15:40:00', '2026-01-18 16:05:00', 'ABANDONED',  6,   0),
-(4, '2026-01-23 10:00:00', '2026-01-23 10:14:00', 'WIN',        9,  80),
-(5, '2026-01-24 09:30:00', NULL,                  'ABANDONED',         0,   0),
-(1,'2026-01-22 18:10:00', '2026-01-22 18:12:00', 'WIN', 10, 120),
-(1, '2026-01-20 18:10:00', '2026-01-20 18:22:00', 'WIN',       12, 120),
-(1, '2026-01-22 21:00:00', NULL,                  'ABANDONED',         0,   0),
-(2, '2026-01-21 19:05:00', '2026-01-21 19:19:00', 'LOSE',      10,  30),
-(3, '2026-01-18 15:40:00', '2026-01-18 16:05:00', 'ABANDONED',  6,   0),
-(4, '2026-01-23 10:00:00', '2026-01-23 10:14:00', 'WIN',        9,  80),
-(5, '2026-01-24 09:30:00', NULL,                  'ABANDONED',         0,   0),
-(1,'2026-01-22 18:10:00', '2026-01-22 18:12:00', 'WIN', 10, 120),
-(1, '2026-01-20 18:10:00', '2026-01-20 18:22:00', 'WIN',       12, 120),
-(1, '2026-01-22 21:00:00', NULL,                  'ABANDONED',         0,   0),
-(2, '2026-01-21 19:05:00', '2026-01-21 19:19:00', 'LOSE',      10,  30),
-(3, '2026-01-18 15:40:00', '2026-01-18 16:05:00', 'ABANDONED',  6,   0),
-(4, '2026-01-23 10:00:00', '2026-01-23 10:14:00', 'WIN',        9,  80),
-(5, '2026-01-24 09:30:00', NULL,                  'ABANDONED',         0,   0),
-(1,'2026-01-22 18:10:00', '2026-01-22 18:12:00', 'WIN', 10, 120);
-
-# Generated_enemies DEMO
-INSERT INTO generated_enemies (match_id, prompt_user_text, enemy_name, enemy_level, stats_json) VALUES
-(1, 'Un bandido ágil de nivel bajo', 'Bandido Veloz', 2,
- JSON_OBJECT('hp', 95, 'attack', 14, 'defense', 4, 'element', 'none')),
-
-(2, 'Una bruja del pantano con veneno', 'Bruja del Fango', 3,
- JSON_OBJECT('hp', 120, 'attack', 16, 'defense', 6, 'element', 'poison')),
-
-(3, 'Un guardián de sombras con guadaña', 'Segador Umbrío', 4,
- JSON_OBJECT('hp', 135, 'attack', 22, 'defense', 7, 'element', 'dark')),
-
-(4, 'Un dragón bebé de fuego', 'Draco Carmesí', 3,
- JSON_OBJECT('hp', 125, 'attack', 19, 'defense', 5, 'element', 'fire')),
-
-(5, 'Un golem de hielo resistente', 'Gólem Glacial', 5,
- JSON_OBJECT('hp', 190, 'attack', 18, 'defense', 14, 'element', 'ice')),
-
-(6, 'Un asesino sigiloso con dagas', 'Acechador Nocturno', 4,
- JSON_OBJECT('hp', 110, 'attack', 24, 'defense', 5, 'element', 'none')),
-
-(7, 'Un bandido ágil de nivel bajo', 'Bandido Veloz', 2,
- JSON_OBJECT('hp', 95, 'attack', 14, 'defense', 4, 'element', 'none')),
-
-(8, 'Una bruja del pantano con veneno', 'Bruja del Fango', 3,
- JSON_OBJECT('hp', 120, 'attack', 16, 'defense', 6, 'element', 'poison')),
-
-(9, 'Un guardián de sombras con guadaña', 'Segador Umbrío', 4,
- JSON_OBJECT('hp', 135, 'attack', 22, 'defense', 7, 'element', 'dark')),
-
-(10, 'Un dragón bebé de fuego', 'Draco Carmesí', 3,
- JSON_OBJECT('hp', 125, 'attack', 19, 'defense', 5, 'element', 'fire')),
-
-(11, 'Un golem de hielo resistente', 'Gólem Glacial', 5,
- JSON_OBJECT('hp', 190, 'attack', 18, 'defense', 14, 'element', 'ice')),
-
-(12, 'Un asesino sigiloso con dagas', 'Acechador Nocturno', 4,
- JSON_OBJECT('hp', 110, 'attack', 24, 'defense', 5, 'element', 'none')),
-
-(13, 'Un bandido ágil de nivel bajo', 'Bandido Veloz', 2,
- JSON_OBJECT('hp', 95, 'attack', 14, 'defense', 4, 'element', 'none')),
-
-(14, 'Una bruja del pantano con veneno', 'Bruja del Fango', 3,
- JSON_OBJECT('hp', 120, 'attack', 16, 'defense', 6, 'element', 'poison')),
-
-(15, 'Un guardián de sombras con guadaña', 'Segador Umbrío', 4,
- JSON_OBJECT('hp', 135, 'attack', 22, 'defense', 7, 'element', 'dark')),
-
-(16, 'Un dragón bebé de fuego', 'Draco Carmesí', 3,
- JSON_OBJECT('hp', 125, 'attack', 19, 'defense', 5, 'element', 'fire')),
-
-(17, 'Un golem de hielo resistente', 'Gólem Glacial', 5,
- JSON_OBJECT('hp', 190, 'attack', 18, 'defense', 14, 'element', 'ice')),
-
-(18, 'Un asesino sigiloso con dagas', 'Acechador Nocturno', 4,
- JSON_OBJECT('hp', 110, 'attack', 24, 'defense', 5, 'element', 'none')),
-(19, 'Un bandido ágil de nivel bajo', 'Bandido Veloz', 2,
- JSON_OBJECT('hp', 95, 'attack', 14, 'defense', 4, 'element', 'none')),
-
-(20, 'Una bruja del pantano con veneno', 'Bruja del Fango', 3,
- JSON_OBJECT('hp', 120, 'attack', 16, 'defense', 6, 'element', 'poison')),
-
-(21, 'Un guardián de sombras con guadaña', 'Segador Umbrío', 4,
- JSON_OBJECT('hp', 135, 'attack', 22, 'defense', 7, 'element', 'dark')),
-
-(22, 'Un dragón bebé de fuego', 'Draco Carmesí', 3,
- JSON_OBJECT('hp', 125, 'attack', 19, 'defense', 5, 'element', 'fire')),
-
-(23, 'Un golem de hielo resistente', 'Gólem Glacial', 5,
- JSON_OBJECT('hp', 190, 'attack', 18, 'defense', 14, 'element', 'ice')),
-
-(24, 'Un asesino sigiloso con dagas', 'Acechador Nocturno', 4,
- JSON_OBJECT('hp', 110, 'attack', 24, 'defense', 5, 'element', 'none')),
-(25, 'Un asesino sigiloso con dagas', 'Acechador Nocturno', 4,
- JSON_OBJECT('hp', 110, 'attack', 24, 'defense', 5, 'element', 'none')),
-(26, 'Un asesino sigiloso con dagas', 'Acechador Nocturno', 4,
- JSON_OBJECT('hp', 110, 'attack', 24, 'defense', 5, 'element', 'none')),
-(27, 'Un asesino sigiloso con dagas', 'Acechador Nocturno', 4,
- JSON_OBJECT('hp', 110, 'attack', 24, 'defense', 5, 'element', 'none')),
-(28, 'Un asesino sigiloso con dagas', 'Acechador Nocturno', 4,
- JSON_OBJECT('hp', 110, 'attack', 24, 'defense', 5, 'element', 'none'));
-
-INSERT INTO battles (
-  match_id, enemy_source, enemy_id, generated_enemy_id, result,
-  turns, damage_dealt, damage_taken
-) VALUES
-(1, 'AI', NULL, 1, 'WIN',       8, 140, 70),
-(2, 'AI', NULL, 2, 'ABANDONED', 3,  35, 20),
-(3, 'AI', NULL, 3, 'LOSE',     11, 120, 160),
-(4, 'AI', NULL, 4, 'WIN',       6, 115, 55),
-(5, 'AI', NULL, 5, 'WIN',      14, 230, 150),
-(6, 'AI', NULL, 6, 'LOSE',      9,  98, 130),
-(7, 'AI', NULL, 1, 'WIN',       8, 140, 70),
-(8, 'AI', NULL, 2, 'ABANDONED', 3,  35, 20),
-(9, 'AI', NULL, 3, 'LOSE',     11, 120, 160),
-(10, 'AI', NULL, 4, 'WIN',       6, 115, 55),
-(11, 'AI', NULL, 5, 'WIN',      14, 230, 150),
-(12, 'AI', NULL, 6, 'LOSE',      9,  98, 130),
-(13, 'AI', NULL, 1, 'WIN',       8, 140, 70),
-(14, 'AI', NULL, 2, 'ABANDONED', 3,  35, 20),
-(15, 'AI', NULL, 3, 'LOSE',     11, 120, 160),
-(16, 'AI', NULL, 4, 'WIN',       6, 115, 55),
-(17, 'AI', NULL, 5, 'WIN',      14, 230, 150),
-(18, 'AI', NULL, 6, 'LOSE',      9,  98, 130),
-(19, 'AI', NULL, 1, 'WIN',       8, 140, 70),
-(20, 'AI', NULL, 2, 'ABANDONED', 3,  35, 20),
-(21, 'AI', NULL, 3, 'LOSE',     11, 120, 160),
-(22, 'AI', NULL, 4, 'WIN',       6, 115, 55),
-(23, 'AI', NULL, 5, 'WIN',      14, 230, 150),
-(24, 'AI', NULL, 6, 'LOSE',      9,  98, 130),
-(25, 'AI', NULL, 1, 'WIN',       8, 140, 70),
-(26, 'AI', NULL, 2, 'ABANDONED', 3,  35, 20),
-(27, 'AI', NULL, 3, 'LOSE',     11, 120, 160),
-(28, 'AI', NULL, 4, 'WIN',       6, 115, 55);
-
-
-
-# Querys - Consultas
-SELECT * FROM users;
-SELECT * FROM characters;
-SELECT * FROM matches;
-SELECT * FROM battles;
-
-# DROPS
-DROP TABLE users;
-DROP TABLE characters;
-DROP TABLE matches;
-DROP Table battles;
-
-
-
+ALTER TABLE generated_enemies
+  ADD UNIQUE KEY uq_generated_enemies_match_id (match_id);
 
